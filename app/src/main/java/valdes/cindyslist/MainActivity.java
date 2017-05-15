db2daa3e1f69c1e085b62c3893a2ef4d9d04b753
valdeslab <dvjr22@gmail.com>
2017-05-15 10:50:41 -0400

    Added MainFragment and res file. Modified MainActivity to use fragment.

--- app/src/main/java/valdes/cindyslist/MainActivity.java
+++ app/src/main/java/valdes/cindyslist/MainActivity.java
@@ -1,5 +1,7 @@
 package valdes.cindyslist;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,6 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = MainFragment.newInstance();
+        fragmentManager.beginTransaction().
+                replace(R.id.main_activity_fragment_container, fragment).commit();
+
     }
 }
 
